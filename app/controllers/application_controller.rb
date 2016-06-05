f545733c6fd03f69b736ff8b5c05bb86104f27be
Michael Goldman <goldmandrummer@gmail.com>
2016-06-05 18:43:59 -0400

    fix javascript spaghetti code... IN PROGRESS cleaning up (introduced some errors)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  before_action :the_stations
   protect_from_forgery with: :exception
+  
+  def the_stations
+    @bikestations = Bikestation.all
+  end
 end
