9ad54100c897023b1d9b0f3be4e8bb187f8e8ca8
LiberalOtaku <ledgam3r1279@gmail.com>
2016-06-12 21:48:36 -0400

    Partial script and CSS for trip page added.

--- js/trip.js
+++ js/trip.js
@@ -1,21 +1,49 @@
 var trip = {
   init: function(formSelector) {
-    this.form = document.querySelector(formSelector);
+    this.form = $(formSelector);
     this.setupEventListeners();
   },
 
   setupEventListeners: function() {
-    this.form.onsubmit = this.submitForm.bind(this);
+    this.form.submit(this.submitForm.bind(this));
   },
 
   submitForm: function(event) {
     event.preventDefault();
-    var trips = document.querySelector('div.trip-area');
-    trips.innerHTML += '';
 
+    // get list of routes and times
 
+    $('div.trip-area').append(trip.buildRouteInfo(bus, bike, walk));
   },
 
+  buildRouteInfo: function(bus, bike, walk) {
+    var busTime = $('<li/>').text("Bus Time: " + bus + " Minutes");
+    var bikeTime = $('<li/>').text("Biking Time: " + bike + " Minutes");
+    var walkTime = $('<li/>').text("Walking Time: " + walk + " Minutes");
+
+    var ul = $('<ul/>').slideUp()
+      .attr({
+        "class": "trip-details",
+        "show": "false",
+      })
+      .append(busTime, bikeTime, walkTime);
+
+    var button = $('<a/>').attr({
+      href: "#",
+    }).text("Show Details")
+      .click(function() {
+      if (ul.attr("show")) {
+        ul.slideUp().attr({ "show": "false", });
+        button.text("Show Details");
+      }
+      else {
+        ul.slideDown().attr({ "show": "true", });
+        button.text("Hide Details");
+      }
+    });
+
+    return $('<div/>').append(button, ul);
+  },
 };
 
 trip.init('#tripForm');
